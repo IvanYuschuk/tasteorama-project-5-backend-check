ad55452e67db685b203999b2acee0d4eb7e865ad
hiper <ivan.yuschuk77@gmail.com>
2026-06-20 15:53:38 +0300

    all

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:3001',
-      'https://tasteorama-project5-frontend-check-pbhmjf35a.vercel.app',
+      'https://tasteorama-project5-frontend-check.vercel.app',
     ],
     credentials: true,
   }),
